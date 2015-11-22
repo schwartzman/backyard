ce802f7e7586ed860abadcc4a29f164633bb18b2
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2015-11-23 01:00:44 +0300

    Footer mostly done

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
+			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+
+				<div id="inner-footer" class="wrap cf">
+					<p class="ticulars"><span>7:30 to 4:30 every day</span><span>078 751 715</span><span><a>11B Street 246 &#10178; Sothearos, Phnom Penh</a></span></p>
+					<p class="octopus"><a href="https://facebook.com/backyardeats" target="_blank"><i class="fa fa-facebook-square"></i></a><a href="https://instagram.com/backyardeats/" target="_blank"><i class="fa fa-instagram"></i></a></p>
+					<p class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></p>
+				</div>
+
+			</footer>
+
 		</div>
 
 		<?php // all js scripts are loaded in library/bones.php ?>
