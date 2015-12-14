885f2d10a8fb3594e31cad7257532d2e981f8a2a
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2015-12-15 00:30:22 +0700

    Footer edits

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
 			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
 				<div id="inner-footer" class="wrap cf">
-					<p class="ticulars"><span>7:30 to 4:30 every day</span><span>078 751 715</span><span><a>11B Street 246 &#10178; Sothearos, Phnom Penh</a></span></p>
+					<p class="ticulars"><span>7:30 to 4:30 every day</span><span>078 751 715</span><span><a target="_blank" href="https://www.google.com/maps/place/Backyard+Cafe/@11.561252,104.934377,16z/data=!4m2!3m1!1s0x0:0xb85128820699c627">11B Street 246, Phnom Penh</a></span></p>
 					<p class="octopus"><a href="https://facebook.com/backyardeats" target="_blank"><i class="fa fa-facebook-square"></i></a><a href="https://instagram.com/backyardeats/" target="_blank"><i class="fa fa-instagram"></i></a></p>
-					<p class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></p>
 				</div>
 
 			</footer>
