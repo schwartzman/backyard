c271afbd3ef7b05cdffdd7a26d7ebe7ba3011355
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2015-12-15 13:08:12 +0700

    Add TripAdvisor icon to footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 				<div id="inner-footer" class="wrap cf">
 					<p class="ticulars"><span>7:30 to 4:30 every day</span><span>078 751 715</span><span><a target="_blank" href="https://www.google.com/maps/place/Backyard+Cafe/@11.561252,104.934377,16z/data=!4m2!3m1!1s0x0:0xb85128820699c627">11B Street 246, Phnom Penh</a></span></p>
-					<p class="octopus"><a href="https://facebook.com/backyardeats" target="_blank"><i class="fa fa-facebook-square"></i></a><a href="https://instagram.com/backyardeats/" target="_blank"><i class="fa fa-instagram"></i></a></p>
+					<p class="octopus"><a href="https://facebook.com/backyardeats" target="_blank"><i class="fa fa-facebook-square"></i></a><a href="https://instagram.com/backyardeats/" target="_blank"><i class="fa fa-instagram"></i></a><a href="http://www.tripadvisor.com/Restaurant_Review-g293940-d7385168-Reviews-Backyard_Cafe-Phnom_Penh.html" target="_blank"><i class="fa fa-tripadvisor"></i></a></p>
 				</div>
 
 			</footer>
--- library/enqueue.php
+++ library/enqueue.php
@@ -3,7 +3,7 @@ namespace backyard;
 
 function enqueue(){
     wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900' );
-    wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css' );
+    wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' );
 }
 
 add_action( 'wp_enqueue_scripts', 'backyard\enqueue' );
