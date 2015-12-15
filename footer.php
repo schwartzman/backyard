88d33136cb462b819dc9952c27eeb98e0ab0adb2
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2015-12-16 00:00:16 +0700

    Footer content edits

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
 				<div id="inner-footer" class="wrap cf">
-					<p class="ticulars"><span>7:30 to 4:30 every day</span><span>078 751 715</span><span><a target="_blank" href="https://www.google.com/maps/place/Backyard+Cafe/@11.561252,104.934377,16z/data=!4m2!3m1!1s0x0:0xb85128820699c627">11B Street 246, Phnom Penh</a></span></p>
+					<p class="ticulars"><a href="/hours-location/"><span>7:30 to 4:30 M-Th</span><span>7:30 to 9:00 F-S-S</span><span>+855 078 751 715</span><span>11B Street 246, Phnom Penh, Cambodia</span></a></p>
 					<p class="octopus"><a href="https://facebook.com/backyardeats" target="_blank"><i class="fa fa-facebook-square"></i></a><a href="https://instagram.com/backyardeats/" target="_blank"><i class="fa fa-instagram"></i></a><a href="http://www.tripadvisor.com/Restaurant_Review-g293940-d7385168-Reviews-Backyard_Cafe-Phnom_Penh.html" target="_blank"><i class="fa fa-tripadvisor"></i></a></p>
 				</div>
 
