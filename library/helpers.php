183cb94fbdcecd62b4e74f90cc86934573be1dcb
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2015-12-15 16:59:17 +0700

    Add ACF tracking

--- library/helpers.php
+++ library/helpers.php
@@ -20,5 +20,20 @@ function add_slug_body_class( $classes ) {
 	}
 	return $classes;
 }
+
+function acf_json_save_point( $path ) {
+    $path = get_stylesheet_directory() . '/library/acf';
+    return $path;
+}
+
+function acf_json_load_point( $paths ) {
+    // remove original path (optional)
+    unset($paths[0]);
+    $paths[] = get_stylesheet_directory() . '/library/acf';
+    return $paths;
+}
+
 add_filter( 'body_class', 'backyard\add_slug_body_class' );
+add_filter('acf/settings/save_json', 'backyard\acf_json_save_point');
+add_filter('acf/settings/load_json', 'backyard\acf_json_load_point');
 ?>
