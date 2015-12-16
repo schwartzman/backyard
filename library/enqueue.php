40e16b8bb3f86ae98285c0c4f8b477b770fa1c8e
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2015-12-16 15:09:03 +0700

    Split body/header typefaces

--- library/enqueue.php
+++ library/enqueue.php
@@ -2,7 +2,7 @@
 namespace backyard;
 
 function enqueue(){
-    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900' );
+    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Quattrocento+Sans:400,400italic,700|Raleway:400,100,200,300,500,600,700,800,900' );
     wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' );
 }
 
