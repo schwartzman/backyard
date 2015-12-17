0b8538eb7ea9ee4339f8c3dd0eb3dab8794a9da0
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2015-12-17 17:24:11 +0700

    Minor stock scripts cleanup

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -1,15 +1,6 @@
 /*
  * Bones Scripts File
  * Author: Eddie Machado
- *
- * This file should contain any js scripts you want to add to the site.
- * Instead of calling it in the header or throwing it inside wp_head()
- * this file will be called automatically in the footer so as not to
- * slow the page load.
- *
- * There are a lot of example functions and tools in here. If you don't
- * need any of it, just remove it. They are meant to be helpers and are
- * not required. It's your world baby, you can do whatever you want.
 */
 
 
@@ -86,23 +77,6 @@ var timeToWaitForLast = 100;
  * Keep it light and always make sure the larger viewports are doing the heavy lifting.
  *
 */
-
-/*
- * We're going to swap out the gravatars.
- * In the functions.php file, you can see we're not loading the gravatar
- * images on mobile to save bandwidth. Once we hit an acceptable viewport
- * then we can swap out those images since they are located in a data attribute.
-*/
-function loadGravatars() {
-  // set the viewport using the function above
-  viewport = updateViewportDimensions();
-  // if the viewport is tablet or larger, we load in the gravatars
-  if (viewport.width >= 768) {
-  jQuery('.comment img[data-gravatar]').each(function(){
-    jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
-  });
-	}
-} // end function
 
 
 /*
