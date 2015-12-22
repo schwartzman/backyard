6e6d02049098fe97439de6e77fe4345b10b306b9
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2015-12-22 12:25:17 +0700

    Reduce unused font variants

--- library/enqueue.php
+++ library/enqueue.php
@@ -4,7 +4,7 @@ namespace backyard;
 function enqueue(){
 	wp_enqueue_script( 'jquery-masonry', false, array('jquery'), false, true );
 	
-    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Quattrocento+Sans:400,400italic,700|Raleway:400,100,200,300,500,600,700,800,900' );
+    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Quattrocento+Sans:400,700|Raleway:300,400,500,600' );
     wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' );
 }
 
