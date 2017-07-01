a7e3c2bbc4d5768e5fbf39c056afd1c0daa543bc
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2017-07-01 17:43:44 +0530

    Swap cleanse form (correctly)

--- page.php
+++ page.php
@@ -17,29 +17,28 @@
 								</header> <?php // end article header ?>
 
 								<section class="entry-content cf" itemprop="articleBody">
-								<?php if (is_page('cleanses')) : ?>
-									<section class="entry-content cf cleanse-form">
-										<p class="nb">All fields are required.</p>
-										<div id="wufoo-z1afgx0u19zpsxn">
-										Pleae fill out our <a href="https://emmafountain.wufoo.eu/forms/z1afgx0u19zpsxn">cleanse order form</a>.
+									<?php if (is_page('contact')) : ?>
+										<div class="contact-form">
+											<div id="wufoo-wxvatyd1u3dz4a">
+											Please fill out our <a href="https://emmafountain.wufoo.com/forms/wxvatyd1u3dz4a">contact form</a>.
+											</div>
+											<script type="text/javascript">var wxvatyd1u3dz4a;(function(d, t) {
+											var s = d.createElement(t), options = {
+											'userName':'emmafountain',
+											'formHash':'wxvatyd1u3dz4a',
+											'autoResize':true,
+											'height':'577',
+											'async':true,
+											'host':'wufoo.com',
+											'header':'hide',
+											'ssl':true};
+											s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+											s.onload = s.onreadystatechange = function() {
+											var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+											try { wxvatyd1u3dz4a = new WufooForm();wxvatyd1u3dz4a.initialize(options);wxvatyd1u3dz4a.display(); } catch (e) {}};
+											var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+											})(document, 'script');</script>
 										</div>
-										<script type="text/javascript">var z1afgx0u19zpsxn;(function(d, t) {
-										var s = d.createElement(t), options = {
-										'userName':'emmafountain',
-										'formHash':'z1afgx0u19zpsxn',
-										'autoResize':true,
-										'height':'1651',
-										'async':true,
-										'host':'wufoo.eu',
-										'header':'show',
-										'ssl':true};
-										s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.eu/scripts/embed/form.js';
-										s.onload = s.onreadystatechange = function() {
-										var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
-										try { z1afgx0u19zpsxn = new WufooForm();z1afgx0u19zpsxn.initialize(options);z1afgx0u19zpsxn.display(); } catch (e) {}};
-										var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-										})(document, 'script');</script>
-									</section>
 									<?php endif; ?>
 									<?php if (is_page('hours-location')) : ?>
 										<div class="map">
