46b61043d70c4f0bcc04535f6b3cae53bee0c4a0
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2017-08-01 13:55:02 +0700

    Change opening hours

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
 				<div id="inner-footer" class="wrap cf">
-					<p class="ticulars"><a href="/hours-location/"><span>7:30 to 4:30 M</span><span>7:30 to 8:00 Tu-Su</span><span>+855 078 751 715</span><span>11B Street 246, Phnom Penh, Cambodia</span></a></p>
+					<p class="ticulars"><a href="/hours-location/"><span>7:30 to 4:30 M</span><span>7:30 to 9:00 Tu-Su</span><span>+855 078 751 715</span><span>11B Street 246, Phnom Penh, Cambodia</span></a></p>
 					<p class="octopus"><a href="https://facebook.com/backyardeats" target="_blank"><i class="fa fa-facebook-square"></i></a><a href="https://instagram.com/backyardeats/" target="_blank"><i class="fa fa-instagram"></i></a><a href="http://www.tripadvisor.com/Restaurant_Review-g293940-d7385168-Reviews-Backyard_Cafe-Phnom_Penh.html" target="_blank"><i class="fa fa-tripadvisor"></i></a></p>
 				</div>
 
